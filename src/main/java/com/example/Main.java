3a7e70df17d06a18d8b348707836be034b9c5152
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2021-02-26 14:40:47 +0300

    IDEA-260360

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 100000; i++) {
+            System.out.println(i);
+            Thread.sleep(100);
+        }
+    }
+}
